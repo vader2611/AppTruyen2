4fa1129c80dff42a1e68d1bf3a4aa5d4fbb95175
Dell <tuanvader2611@gmail.com>
2019-11-13 12:39:51 +0700

    HELLO

---
+++ app/src/main/java/vn/edu/poly/apptruyen/MainActivity.java
@@ -0,0 +1,104 @@
+package vn.edu.poly.apptruyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import vn.edu.poly.apptruyen.adapter.TruyenTranhAdapter;
+import vn.edu.poly.apptruyen.api.ApiLayTruyen;
+import vn.edu.poly.apptruyen.interfaces.LayTruyenVe;
+import vn.edu.poly.apptruyen.object.TruyenTranh;
+
+public class MainActivity extends AppCompatActivity implements LayTruyenVe {
+    GridView gdvDSTruyen;
+    TruyenTranhAdapter adapter;
+    ArrayList<TruyenTranh> truyenTranhArrayList;
+    EditText edtTimKiem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        anhXa();
+        setUp();
+        setClick();
+        new ApiLayTruyen(this).execute();
+    }
+    private void init(){
+        truyenTranhArrayList= new ArrayList<>();
+
+        adapter= new TruyenTranhAdapter(this,0, truyenTranhArrayList);
+    }
+    private void anhXa(){
+        gdvDSTruyen= findViewById(R.id.gdvDSTruyen);
+        edtTimKiem= findViewById(R.id.edtTimKiem);
+    }
+    private void setUp(){
+        gdvDSTruyen.setAdapter(adapter);
+    }
+    private void setClick(){
+        edtTimKiem.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String s= edtTimKiem.getText().toString();
+                adapter.sortTruyen(s);
+
+            }
+        });
+    }
+
+    @Override
+    public void batDau() {
+        Toast.makeText(this,"Dang Lay Ve", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void ketThuc(String data) {
+        try {
+            truyenTranhArrayList.clear();
+            JSONArray arr = new JSONArray(data);
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject o = arr.getJSONObject(i);
+                truyenTranhArrayList.add(new TruyenTranh(o));
+            }
+            adapter= new TruyenTranhAdapter(this,0, truyenTranhArrayList);
+            gdvDSTruyen.setAdapter(adapter);
+        } catch (JSONException e) {
+
+        }
+    }
+
+    @Override
+    public void biLoi() {
+        Toast.makeText(this,"Loi Ket Noi", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void update(View view) {
+        new ApiLayTruyen(this).execute();
+    }
+}
